93d5fff28a2247b12664ca78f4d7ed8b76ee005e
Alex <bozeak@gmail.com>
2018-12-10 10:12:06 +0200

    EASYOPAC-999 - CUstom field for preferences.

---
+++ src/Plugin/Field/FieldType/PreferencesSetFieldType.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace Drupal\emailservice\Plugin\Field\FieldType;
+
+use Drupal\Core\Field\FieldItemBase;
+use Drupal\Core\Field\FieldStorageDefinitionInterface;
+use Drupal\Core\StringTranslation\TranslatableMarkup;
+use Drupal\Core\TypedData\DataDefinition;
+
+/**
+ * Plugin implementation of the 'preferences_set_field_type' field type.
+ *
+ * @FieldType(
+ *   id = "preferences_set_field_type",
+ *   label = @Translation("Preferences Set"),
+ *   description = @Translation("Preferences Set field"),
+ *   default_widget = "preferences_set_widget",
+ *   default_formatter = "preferences_set_formatter"
+ * )
+ */
+class PreferencesSetFieldType extends FieldItemBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function propertyDefinitions(FieldStorageDefinitionInterface $field_definition) {
+    // Prevent early t() calls by using the TranslatableMarkup.
+    $properties['label'] = DataDefinition::create('string')
+      ->setLabel(new TranslatableMarkup('Preference label'))
+      ->setRequired(TRUE);
+
+    $properties['machine_name'] = DataDefinition::create('string')
+      ->setLabel(new TranslatableMarkup('Preference machine name'))
+      ->setRequired(TRUE);
+
+    $properties['cql_query'] = DataDefinition::create('string')
+      ->setLabel(new TranslatableMarkup('Preference CQL Query'))
+      ->setRequired(TRUE);
+
+    $properties['status'] = DataDefinition::create('integer')
+      ->setLabel(new TranslatableMarkup('Preference status'))
+      ->setRequired(TRUE);
+
+    return $properties;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function schema(FieldStorageDefinitionInterface $field_definition) {
+    $schema = [
+      'columns' => [
+        'label' => [
+          'type' => 'varchar',
+          'description' => '',
+          'length' => 255,
+          'not null' => TRUE,
+          'default' => '',
+        ],
+        'machine_name' => [
+          'type' => 'varchar',
+          'description' => '',
+          'length' => 255,
+          'not null' => TRUE,
+          'default' => '',
+        ],
+        'cql_query' => [
+          'type' => 'varchar',
+          'description' => '',
+          'length' => 255,
+          'not null' => TRUE,
+          'default' => '',
+        ],
+        'status' => [
+          'type' => 'int',
+          'description' => '',
+          'not null' => TRUE,
+          'default' => 1,
+        ],
+      ],
+    ];
+
+    return $schema;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function isEmpty() {
+    $fields = [
+      'label',
+      'machine_name',
+      'cql_query',
+      'status',
+    ];
+
+    foreach ($fields as $field) {
+      $value = $this->get($field)->getValue();
+      return $value === NULL || $value === '';
+    }
+  }
+
+  public function preSave() {
+    $prefix = '999999';
+    $machine_name = preg_replace('@[^a-z0-9-]+@', '-', strtolower($this->getValue()['label']));
+    $this->set('machine_name', $prefix . '_' . $machine_name);
+  }
+
+  public function postSave($update) {
+    // Fetch entity.
+    $entity = $this->getEntity();
+
+    // Get current entity_id.
+    $entity_id = $entity->get('nid')->getValue();
+
+    // Get preferences.
+    $preference = $this->getValue();
+
+    // Extend preferences array.
+    $db_pref = [
+        'entity_id' => $entity_id[0]['value'],
+        'preference_type' => $this->getFieldDefinition()->getName(),
+      ] + $preference;
+
+    $connection = \Drupal::database();
+    $connection->merge('emailservice_preferences_mapping')
+      ->key('machine_name', $db_pref['machine_name'])
+      ->fields($db_pref)
+      ->execute();
+  }
+
+}
---
+++ src/Plugin/Field/FieldWidget/PreferencesSetWidget.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Drupal\emailservice\Plugin\Field\FieldWidget;
+
+use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Field\WidgetBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Plugin implementation of the 'preferences_set_widget' widget.
+ *
+ * @FieldWidget(
+ *   id = "preferences_set_widget",
+ *   label = @Translation("Preferences set widget"),
+ *   field_types = {
+ *     "preferences_set_field_type"
+ *   }
+ * )
+ */
+class PreferencesSetWidget extends WidgetBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function defaultSettings() {
+    return parent::defaultSettings();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsForm(array $form, FormStateInterface $form_state) {
+    return parent::settingsForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsSummary() {
+    $summary = [];
+
+    return $summary;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function formElement(FieldItemListInterface $items, $delta, array $element, array &$form, FormStateInterface $form_state) {
+
+    $item =& $items[$delta];
+
+    $element = [
+      '#type' => 'fieldset',
+      '#title' => t('Preferences Set'),
+      '#open' => TRUE,
+    ];
+
+    $element['label'] = [
+      '#type' => 'textfield',
+      '#title' => t('Label'),
+      '#default_value' => isset($item->label) ? $item->label : NULL,
+    ];
+
+    $element['machine_name'] = [
+      '#type' => 'hidden',
+      '#value' => isset($item->machine_name) ? $item->machine_name : 'stub',
+    ];
+
+    $element['cql_query'] = [
+      '#type' => 'textfield',
+      '#title' => t('CQL Query'),
+      '#default_value' => isset($item->cql_query) ? $item->cql_query : NULL,
+    ];
+
+    $element['status'] = [
+      '#type' => 'hidden',
+      '#default_value' => isset($item->status) ? $item->status : 1,
+    ];
+
+    return $element;
+  }
+
+}
+
