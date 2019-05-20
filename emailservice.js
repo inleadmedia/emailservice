5a1acaf9c74b0cf5c9f105de8923fb67786a5230
Alex <bozeak@gmail.com>
2019-05-20 17:41:58 +0300

    EMAIL-23 - Make JS compatible with IE browser.

--- emailservice.js
+++ emailservice.js
@@ -10,6 +10,51 @@ var throbber = document.getElementsByClassName('loader');
 var checkboxes = document.getElementsByClassName('form-checkbox');
 var update_button = document.getElementsByName('update');
 
+// Define prototype of find() function for IE.
+if (!Array.prototype.find) {
+  Object.defineProperty(Array.prototype, 'find', {
+    value: function(predicate) {
+      // 1. Let O be ? ToObject(this value).
+      if (this == null) {
+        throw new TypeError('"this" is null or not defined');
+      }
+
+      var o = Object(this);
+
+      // 2. Let len be ? ToLength(? Get(O, "length")).
+      var len = o.length >>> 0;
+
+      // 3. If IsCallable(predicate) is false, throw a TypeError exception.
+      if (typeof predicate !== 'function') {
+        throw new TypeError('predicate must be a function');
+      }
+
+      // 4. If thisArg was supplied, let T be thisArg; else let T be undefined.
+      var thisArg = arguments[1];
+
+      // 5. Let k be 0.
+      var k = 0;
+
+      // 6. Repeat, while k < len
+      while (k < len) {
+        // a. Let Pk be ! ToString(k).
+        // b. Let kValue be ? Get(O, Pk).
+        // c. Let testResult be ToBoolean(? Call(predicate, T, « kValue, k, O »)).
+        // d. If testResult is true, return kValue.
+        var kValue = o[k];
+        if (predicate.call(thisArg, kValue, k, o)) {
+          return kValue;
+        }
+        // e. Increase k by 1.
+        k++;
+      }
+
+      // 7. Return undefined.
+      return undefined;
+    }
+  });
+}
+
 if (subscribe_button.length !== 0 || update_button.length !== 0) {
   // Disabling subscription button by default.
   if (subscribe_button.length !== 0) {
@@ -49,7 +94,7 @@ if (subscribe_button.length !== 0 || update_button.length !== 0) {
           var response = JSON.parse(this.response);
 
           // Remove loader when request is finished.
-          loader.remove();
+          loader.outerHTML = '';
 
           // Process 'status' values.
           switch (response.status) {
