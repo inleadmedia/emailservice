ced055cca2dc53e4b1c88bbeccbdf11a5c720e74
Dinu Rodnitchi <rodnitchi@gmail.com>
2025-09-15 22:05:55 +0300

    NPH-67: Decrease the step for search query.

--- src/Services/LmsRequestService.php
+++ src/Services/LmsRequestService.php
@@ -118,7 +118,7 @@ class LmsRequestService {
 
     $results = [];
     foreach ($filteredCategories as $category) {
-      $query = "/search?query=(($category->field_types_cql_query_value) AND ($category->cql_query)) AND term.acSource=\"bibliotekskatalog\" AND holdingsitem.accessionDate>=\"NOW-7DAYS\"&step=30&_source=emailservice";
+      $query = "/search?query=(($category->field_types_cql_query_value) AND ($category->cql_query)) AND term.acSource=\"bibliotekskatalog\" AND holdingsitem.accessionDate>=\"NOW-7DAYS\"&step=15&_source=emailservice";
       $uri = $this->lmsServiceURL . $alias . $query;
 
       try {
